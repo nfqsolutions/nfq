fbd9e9c3954aa31503351b392e59769b667d4e63
Guillem Borrell <guillemborrell@gmail.com>
2016-11-14 09:45:42 +0100

    Set zip_safe to setup and version bump.

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 setup(
     name="nfq",
     description="NFQ Solutions base package",
-    version="0.1",
+    version="0.1.1",
     author="NFQ Solutions",
     author_email="solutions@nfq.es",
     packages=[
         'nfq',
         ],
+    zip_safe=False,
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
     )
